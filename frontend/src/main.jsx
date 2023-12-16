b8a92a7695f451b672b98b1fb36e084ab4a7451b
Diegoj95 <diegojimenez377@gmail.com>
2023-12-16 18:33:48 -0300

    reactDom de modulos, formularios

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+// main.jsx
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import App from './App.jsx';
+import AdmArticulos from './pages/AdmArticulos';
+import IngresoArticulos from './pages/IngresoArticulos';
+import TraspasoArticulos from './pages/TraspasoArticulos';
+import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/administrar-articulos" element={<AdmArticulos />} />
+        <Route path="/ingreso-articulos" element={<IngresoArticulos />} />
+        <Route path="/traspaso-articulos" element={<TraspasoArticulos />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
-)
+);
